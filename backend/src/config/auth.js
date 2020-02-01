1dfa6d55b2cfbbdff978c51fb0193f37fbc630c1
Henrique Pagno <henriquepagno@gmail.com>
2020-02-01 18:43:59 -0300

    Created user and session controllers

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'afc6a176040f58de96ad250e6ee3c0e2',
+  expiresIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hellooooo' }));
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// Defines global authorization middleware to the following routes below.
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
