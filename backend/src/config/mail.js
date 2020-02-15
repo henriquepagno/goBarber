5e9c4fb826517a68e1164682c699d5f333f9a425
Henrique Pagno <henriquepagno@gmail.com>
2020-02-15 15:36:56 -0300

    Created cancellation mail and queue funciontalities

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '861314fd72473e',
+    pass: '122162db9193f0',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com>',
+  },
+};
